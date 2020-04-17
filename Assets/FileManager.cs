238685f9a7c2789493050b9ce25730534a3516f0
Aly Samy Elwazzan <aly.elwazzan@gmail.com>
2020-04-17 14:18:42 +0200

    initial

---
+++ Assets/FileManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class FileManager : MonoBehaviour
+{
+    string path;
+    public RawImage Image;
+
+    public  void OpenExplorer ()
+    {
+        path = EditorUtility.OpenFilePanel("Overwrite with png", "", "png");
+        GetImage();
+    }
+
+    void GetImage()
+    {
+        if (path!=null)
+
+        {
+            UpdateImage();
+        }   
+    
+    }
+
+    void UpdateImage()
+    {
+        WWW www = new WWW("file:///" + path);
+        Image.texture = www.texture;
+    }
+}
